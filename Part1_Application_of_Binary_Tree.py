518489ec32a8e2cd049853c864e53152166b1a01
[chrltt-q] <[charlottequezada17@gmail.com]>
2023-01-14 16:11:46 +0800

    class BinarySearchTreeNode

--- Part1_Application_of_Binary_Tree.py
+++ Part1_Application_of_Binary_Tree.py
@@ -2,3 +2,10 @@ print("******** PROGRAMMED BY ********")
 print("****** Charlotte Quezada ******")
 print("********** BSCOE 2-2 **********")
 print("*** Sir Danilo Madrigalejos ***\n")
+
+
+class BinarySearchTreeNode:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
